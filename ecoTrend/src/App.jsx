a2a6475ce5ccb52a19352890d5db31729993875b
SniOpy <soufianeibna@gmail.com>
2025-04-16 17:18:58 +0100

    🚚 F01 : FRONT > APP > Installation react-router-dom  & Create <LoginPage/>

--- ecoTrend/src/App.jsx
+++ ecoTrend/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+    </Routes>
+  );
+}
 
 export default App;
