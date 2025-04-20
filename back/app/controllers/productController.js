a0ccf30a348b93e777d88a4a47acf92f06160a58
SniOpy <soufianeibna@gmail.com>
2025-04-21 00:43:35 +0100

    🚧 F02: BE > PRODUCT > aDD getAllProducts in Datamapper& findAllProducts in controller

--- back/app/controllers/productController.js
+++ back/app/controllers/productController.js
@@ -1,8 +1,17 @@
 const productDatamapper = require('../datamapper/productDatamapper');
 
 const productController = {
-  findProducts: (req, res) => {
-    res.json('Find products');
+  findProducts: async (req, res) => {
+    try {
+      const productsFounded = await productDatamapper.getAllProducts();
+
+      if (productsFounded) {
+        res.status(200).json(productsFounded);
+      }
+    } catch (error) {
+      console.error("Aucun produit n'a été trouvé", error);
+      res.status(404).json({ message: "Aucun produit n'a été trouvé" });
+    }
   },
 };
 
--- back/app/datamapper/productDatamapper.js
+++ back/app/datamapper/productDatamapper.js
@@ -1,3 +1,16 @@
-const productDatamapper = {};
+const client = require('../services/clientPg');
+
+const productDatamapper = {
+  getAllProducts: async () => {
+    try {
+      const query = 'SELECT * FROM "product";';
+
+      const products = await client.query(query);
+      return products.rows;
+    } catch (error) {
+      return console.error('Aucun produit ne correspond à cette requête');
+    }
+  },
+};
 
 module.exports = productDatamapper;
