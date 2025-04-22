b72b4b9e657c06c4a1dcd5f60144378be05d98ff
SniOpy <soufianeibna@gmail.com>
2025-04-22 02:02:34 +0100

    ⚡️ F02 : Get product category with json_build_object and sql request

--- back/app/datamapper/productDatamapper.js
+++ back/app/datamapper/productDatamapper.js
@@ -14,13 +14,27 @@ const productDatamapper = {
 
   getProductById: async (id) => {
     try {
-      const query = 'SELECT * FROM "product" WHERE "id"=$1';
+      const query = `
+        SELECT 
+          p.*, 
+          json_build_object('id', c.id, 'name_category', c.name_category) as category
+        FROM 
+          "product" p
+        LEFT JOIN 
+          "category" c
+        ON 
+          p."category_id" = c."id"
+        WHERE 
+          p."id" = $1;
+      `;
       const value = [id];
 
       const products = await client.query(query, value);
+
       return products.rows[0];
     } catch (error) {
-      return console.error('Aucun produit ne correspond à cet identifiant');
+      console.error('Erreur lors de la récupération du produit :', error.message);
+      return null;
     }
   },
 };
