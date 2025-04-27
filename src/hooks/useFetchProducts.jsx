858f3f5b312e0020cfe762974b738b9e2ca41a42
SniOpy <soufianeibna@gmail.com>
2025-04-27 17:29:40 +0100

    Fix: Cors -> origin : vercel link on app.js

--- src/hooks/useFetchProducts.jsx
+++ src/hooks/useFetchProducts.jsx
@@ -3,17 +3,23 @@ import { useEffect, useState } from 'react';
 
 export const useFetchProducts = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/products`, { withCredentials: true })
       // .get(`http://localhost:3000/products`, { withCredentials: true })
       .then((res) => {
-        console.log('Données reçues :', res.data); // Vérification
         setProducts(res.data);
+        setLoading(false);
       })
-      .catch((err) => console.error('Erreur connexion', err));
+      .catch((err) => {
+        console.error('Erreur connexion', err);
+        setError(err);
+        setLoading(false);
+      });
   }, []);
 
-  return { products };
+  return { products, loading, error };
 };
